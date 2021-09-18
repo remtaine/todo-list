b20f4212e94777e33c04c316da298de6b1064df3
Benjo Kho <benjokho@gmail.com>
2021-09-18 17:58:26 +0800

    change variable names

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,16 +1,28 @@
 import '../styles/main.scss';
+import setupNavHeaders from './layout/nav.js';
+import {todoListItem, todoList} from './component/list.js';
 
-const checkboxes = document.getElementsByTagName('checkbox');
+const checkboxes = document.getElementsByTagName('input');
 
-
-
-
-window.onload = function() {
-    for (let i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].addEventListener('change', function() {
-            if (checkboxes[i].checked) {
-                console.log("OPEN!");
-            }
-        });
+const strikethroughText = function(box) {
+    if (box.checked) {
+        box.parentElement.classList.add('done');
+    }
+    else {
+        box.parentElement.classList.remove('done');
     }
 };
+
+const setupStrikethroughText = function() {
+    for (let i = 0; i < checkboxes.length; i++) {
+        strikethroughText(checkboxes[i]);
+    
+        checkboxes[i].addEventListener('change', function() {
+            strikethroughText(checkboxes[i]);
+        });
+    }
+}
+
+//main starts here
+setupStrikethroughText();
+setupNavHeaders();
