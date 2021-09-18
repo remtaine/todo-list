9e91c8676073ec4687ee15d827856540a3920b13
Benjo Kho <benjokho@gmail.com>
2021-09-18 17:57:55 +0800

    Add functions for list

---
+++ src/javascript/component/list.js
@@ -0,0 +1,11 @@
+const todoList = function() {
+    console.log("summertime Chi")
+    return {};
+}
+
+const todoListItem = function() {
+    console.log("I pop the trunk")
+    return {};
+}
+
+export {todoList, todoListItem};
