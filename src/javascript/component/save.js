de4c355ee7b46bfd32d0f5dd065ebb8daf182ded
Benjo Kho <benjokho@gmail.com>
2021-09-24 14:22:27 +0800

    refactor default list data

--- src/javascript/component/save.js
+++ src/javascript/component/save.js
@@ -1,12 +1,13 @@
-import {listItem, listHeader, setupStrikethroughText} from './list.js';
+import {listItem, listHeader, listDescription} from './list.js';
 import {setStorage, getStorage} from './utils.js';
 
 const createInitSession = function() {
     window.localStorage.clear();
     console.log('Initializing...')
-    if (window.localStorage.getItem("listHeaders") === null) {
-        setStorage('listHeaders', []);
+    if (getStorage("listHeaders") === null) {
         console.log("Creating first session data");
+
+        setStorage('listHeaders', []);
 
         listHeader('main').create();
         listHeader('social').create();
@@ -14,32 +15,31 @@ const createInitSession = function() {
         listHeader('groceries').create();
         listHeader('finance').create();
 
-        const mainDescription = "Did you realize that you're a champion in their eyes?";
-        const mainItems = [
-            {
-                checked: false,
-                content: "This is the story of a champion",
-                deadline: "2021-09-24"
-            },
-            {
-                checked: true,
-                content: "Runners on your mark and they pop the guns",
-                deadline: "2021-09-25"
-            },
-            {
-                checked: false,
-                content: "Stand up, stand up, here he comes",
-                deadline: "2021-09-26"
-            },
-            {
-                checked: false,
-                content: "Tell me, what it takes to be number one",
-                deadline: "2021-09-27"
-            }
-        ];
+        listDescription('main', "Did you realize that you're a champion in their eyes?").create();
 
-        setStorage('main' + '-description', mainDescription);
-        setStorage('main' + '-items', mainItems);
+        listItem('main', {
+            checked: false,
+            content: "This is the story of a champion",
+            deadline: "2021-09-24"
+        }).create();
+
+        listItem('main', {
+            checked: true,
+            content: "Runners on your mark and they pop the guns",
+            deadline: "2021-09-25"
+        }).create();
+
+        listItem('main', {
+            checked: false,
+            content: "Stand up, stand up, here he comes",
+            deadline: "2021-09-26"
+        }).create();
+
+        listItem('main', {
+            checked: false,
+            content: "Tell me, what it takes to be number one",
+            deadline: "2021-09-27"
+        }).create();
     }
     else {
         console.log("Retrieving past session data");
